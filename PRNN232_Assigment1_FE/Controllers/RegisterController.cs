21319646f35aec0a4d952b4611b6763e6796e513
HarryHoa <hoahoquocdai82@gmail.com>
2025-05-30 10:20:22 +0700

    update

---
+++ PRNN232_Assigment1_FE/Controllers/RegisterController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PRNN232_Assigment1_FE.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace PRNN232_Assigment1_FE.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public RegisterController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:7252/api/");
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult>Index(SystemAccountModel accountModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(accountModel);
+            }
+            try
+            {
+                var accountData = new 
+                {
+                    accountId = accountModel.AccountId,
+                    accountName = accountModel.AccountName,
+                    accountEmail = accountModel.AccountEmail,
+                    accountPassword = accountModel.AccountPassword,
+                    accountRole = accountModel.AccountRole,
+
+
+                };
+                var jsonContent = JsonSerializer.Serialize(accountData);
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync("SystemAccount/register", content);
+                if (response != null && response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Login", "Index");
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Registration failed, please try again.";
+                    return View(accountModel);
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "An error occurred during registration: " + ex.Message;
+            }
+            return View(accountModel);
+
+        }
+    }
+}
---
+++ PRNN232_Assigment1_FE/Models/SystemAccountModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRNN232_Assigment1_FE.Models
+{
+    public class SystemAccountModel
+    {
+
+        public short AccountId { get; set; }
+        [Required(ErrorMessage = "AccountName is required.")]
+
+        public string AccountName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@FUNewsManagement\.org$",
+                    ErrorMessage = "Email must be in the format @FUNewsManagement.org")]
+        public string AccountEmail { get; set; }
+
+        [Required(ErrorMessage = "AccountRole is required.")]
+        [Range(1, 2, ErrorMessage = "AccountRole must be either 1 (Admin) or 2 (Staff).")]
+        public int? AccountRole { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string AccountPassword { get; set; }
+    }
+}
